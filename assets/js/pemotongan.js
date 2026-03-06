d661ba43e011115817c4222570baf669b179794d
Ahmad Alfian Nizami <ahmad.nizami@ambersof.com>
2026-03-06 09:52:21 +0700

    revisi

--- assets/js/pemotongan.js
+++ assets/js/pemotongan.js
@@ -1,32 +1,34 @@
-document.getElementById('checkAll').addEventListener('change', function() {
-    const checked = this.checked;
-    document.querySelectorAll('.checkItem').forEach(cb => cb.checked = checked);
+document.getElementById("checkAll").addEventListener("change", function () {
+	const checked = this.checked;
+	document
+		.querySelectorAll(".checkItem")
+		.forEach((cb) => (cb.checked = checked));
 });
 
 // Tombol hapus terpilih
-document.getElementById('deleteSelected').addEventListener('click', function() {
-    const selected = [];
-    document.querySelectorAll('.checkItem:checked').forEach(cb => selected.push(cb.value));
+// document.getElementById('deleteSelected').addEventListener('click', function() {
+//     const selected = [];
+//     document.querySelectorAll('.checkItem:checked').forEach(cb => selected.push(cb.value));
 
-    if(selected.length === 0){
-    alert('Pilih data yang ingin dihapus!');
-    return;
-    }
+//     if(selected.length === 0){
+//     alert('Pilih data yang ingin dihapus!');
+//     return;
+//     }
 
-    if(confirm('Hapus data yang terpilih?')){
-    const form = document.createElement('form');
-    form.method = 'POST';
-    form.action = '<?= site_url("pemotongan/delete_multiple") ?>';
+//     if(confirm('Hapus data yang terpilih?')){
+//     const form = document.createElement('form');
+//     form.method = 'POST';
+//     form.action = '<?= site_url("pemotongan/delete_multiple") ?>';
 
-    selected.forEach(id => {
-        const input = document.createElement('input');
-        input.type = 'hidden';
-        input.name = 'id_pemotongan[]';
-        input.value = id;
-        form.appendChild(input);
-    });
+//     selected.forEach(id => {
+//         const input = document.createElement('input');
+//         input.type = 'hidden';
+//         input.name = 'id_pemotongan[]';
+//         input.value = id;
+//         form.appendChild(input);
+//     });
 
-    document.body.appendChild(form);
-    form.submit(); // reload halaman setelah submit
-    }
-});
+//     document.body.appendChild(form);
+//     form.submit(); // reload halaman setelah submit
+//     }
+// });
