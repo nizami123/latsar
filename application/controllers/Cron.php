1afcdc61a9f21f5a77393737d53054f82eda6695
Ahmad Alfian Nizami <ahmad.nizami@ambersof.com>
2026-03-07 11:01:12 +0700

    cron

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -5,7 +5,26 @@ class Cron extends CI_Controller {
 
     public function hitung_populasi($bulan,$tahun)
     {
+
+        // log mulai
+        $this->db->insert('log_cron',[
+            'proses' => 'START hitung populasi',
+            'bulan' => $bulan,
+            'tahun' => $tahun,
+            'waktu' => date('Y-m-d H:i:s')
+        ]);
+
+        // jalankan stored procedure
         $this->db->query("CALL sp_hitung_populasi_bulan('$bulan','$tahun')");
+
+        // log selesai
+        $this->db->insert('log_cron',[
+            'proses' => 'END hitung populasi',
+            'bulan' => $bulan,
+            'tahun' => $tahun,
+            'waktu' => date('Y-m-d H:i:s')
+        ]);
+
     }
 
 }
