72f91314b49d56c39b82f2dcf0d44df53bb93e41
Ahmad Alfian Nizami <ahmad.nizami@ambersof.com>
2026-03-07 11:26:01 +0700

    cron

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -4,9 +4,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Cron extends CI_Controller {
 
     public function insert_populasi()
-    {
+{
+    try {
+
         $bulan = date('m', strtotime('-1 month'));
         $tahun = date('Y', strtotime('-1 month'));
-        $this->db->query("CALL sp_rekap_populasi('".$bulan."', '".$tahun."')"); 
+
+        $this->db->query("CALL sp_rekap_populasi(?, ?)", [$bulan, $tahun]);
+
+        $error = $this->db->error();
+        if ($error['code'] != 0) {
+            throw new Exception($error['message']);
+        }
+
+        echo "Berhasil insert populasi";
+
+    } catch (Exception $e) {
+
+        echo "Error: " . $e->getMessage();
     }
+}
 }
